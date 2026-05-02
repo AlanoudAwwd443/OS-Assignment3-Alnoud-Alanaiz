f472ad96317c58ed260b620284fc60b3680da65f
anod1-art1 <alnwdalnzy556@gmail.com>
2026-05-02 21:25:13 +0300

    todo 4 finally cpuSemaphore

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -185,11 +185,17 @@ class Process implements Runnable {
             }
             System.out.println();
             
-        } finally {
+        }   finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+              SharedResources.cpuSemaphore.release();
+            }
+        catch (InterruptedException e) {
+            System.out.println(Colors.RED + "  ✗ " + name + " semaphore interrupted." + Colors.RESET);
         }
     }
+        
+    
     
     private String createProgressBar(int progress, int width) {
         int filled = (progress * width) / 100;

15d55364e62a084e3d1af2f4de932643bcdda57d
anod1-art1 <alnwdalnzy556@gmail.com>
2026-05-02 21:16:28 +0300

    try  startTime == -1

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -54,6 +54,7 @@ class SharedResources {
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
     // Method to increment context switch counter
+      // Method to increment context switch counter
     public static void incrementContextSwitch() {
         contextSwitchLock.lock();
         try {
@@ -93,32 +94,8 @@ class SharedResources {
         }
     }
 }
-    // Method to increment context switch counter
-    public static void incrementContextSwitch() {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
-    }
+  
     
-    // Method to increment completed process counter
-    public static void incrementCompletedProcess() {
-        // TODO: Protect this critical section with a lock
-        completedProcessCount++;
-    }
-    
-    // Method to add waiting time
-    public static void addWaitingTime(long time) {
-        // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
-    }
-    
-    // Method to log execution
-    public static void logExecution(String message) {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
-    }
-}
 
 // Class representing a process that implements Runnable to be run by a thread
 class Process implements Runnable {
@@ -145,6 +122,12 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
+         try {
+                if (startTime == -1) {
+                    startTime = System.currentTimeMillis();
+                }
+            }
+        
         
         try {
             if (startTime == -1) {
